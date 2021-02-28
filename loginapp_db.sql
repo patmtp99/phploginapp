dd05bdb5ab784c951efbd099c7fe57f66b723ef9
Mr Kpoti <patrickamtepe@gmail.com>
2021-02-28 03:34:37 +0000

    login db working

---
+++ loginapp_db.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE thomas_adams_dw;
+
+use thomas_adams_dw;
+
+CREATE TABLE users(
+	uid INT NOT NULL AUTO_INCREMENT,
+	firstname VARCHAR(55) NOT NULL,
+	surname VARCHAR(55) NOT NULL,
+	username VARCHAR(55) NOT NULL UNIQUE,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	password VARCHAR(200) NOT NULL,
+	dob DATE NOT NULL, 
+	postal_addr VARCHAR(255),
+	PRIMARY KEY(uid)
+);
